2ed811af39d9cb3039bef73322bc93ad6fed8986
lunaboards-dev <30084950+lunaboards-dev@users.noreply.github.com>
2021-05-26 21:37:40 -0500

    overhaul uncpio

--- uncpio/uncpio.lua
+++ uncpio/uncpio.lua
@@ -1,68 +1,49 @@
 local args = {...}
 local file = io.open(args[1], "rb")
 
-local dent = {
-	magic = 0,
-	dev = 0,
-	ino = 0,
-	mode = 0,
-	uid = 0,
-	gid = 0,
-	nlink = 0,
-	rdev = 0,
-	mtime = 0,
-	namesize = 0,
-	filesize = 0,
-}
+local header = "HHHHHHHHHHHHH"
 
-local function readint(amt, rev)
-	local tmp = 0
-	for i=(rev and amt) or 1, (rev and 1) or amt, (rev and -1) or 1 do
-		tmp = tmp | (file:read(1):byte() << ((i-1)*8))
-	end
-	return tmp
-end
+local magic = 0x71c7
 
-local filesystem = filesystem or require("filesystem")
-
-local function fwrite()
-	local dir = dent.name:match("(.+)/.*%.?.+")
-	if (dir) then
-		filesystem.makeDirectory(os.getenv("PWD").."/"..dir)
-	end
-	local hand = io.open(dent.name, "w")
-	hand:write(file:read(dent.filesize))
-	hand:close()
-end
-
+local dent = {}
 while true do
-	dent.magic = readint(2)
-	local rev = false
-	if (dent.magic ~= tonumber("070707", 8)) then rev = true end
-	dent.dev = readint(2)
-	dent.ino = readint(2)
-	dent.mode = readint(2)
-	dent.uid = readint(2)
-	dent.gid = readint(2)
-	dent.nlink = readint(2)
-	dent.rdev = readint(2)
-	dent.mtime = (readint(2) << 16) | readint(2)
-	dent.namesize = readint(2)
-	dent.filesize = (readint(2) << 16) | readint(2)
-	local name = file:read(dent.namesize):sub(1, dent.namesize-1)
-	if (name == "TRAILER!!!") then break end
-	--for k, v in pairs(dent) do
-	--	print(k, v)
-	--end
+	local mgk = file:read(2)
+	local e = "<"
+	if string.unpack("<H", mgk) ~= magic then
+		e = ">"
+	end
+	dent.magic, dent.dev, dent.ino, dent.mode, dent.uid, dent.gid, dent.nlink, dent.rdev,
+	dent.mtime_hi, dent.mtime_lo, dent.namesize, dent.filesize_hi,
+	dent.filesize_lo = string.unpack(e..header, mgk..file:read(header:packsize()-2))
+	if dent.magic ~= magic then
+		error(string.format("bad magic! (%x ~= %x)", dent.magic, magic))
+	end
+	dent.mtime = (dent.mtime_hi << 16) | dent.mtime_lo
+	dent.filesize = (dent.filesize_hi << 16) | dent.filesize_lo
+	local name = file:read(dent.namesize):sub(1, dent.namesize - 1)
+	if name == "TRAILER!!!" then break end
 	dent.name = name
 	print(name)
-	if (dent.namesize % 2 ~= 0) then
-		file:seek("cur", 1)
+	if dent.namesize & 1 > 0 then file:seek("cur", 1) end
+	if (dent.mode & 0xF000 == 0x8000) then
+		local dir = dent.name:match("(.+)/.*%.?.+$")
+		if (dir) then
+			filesystem.makeDirectory(os.getenv("PWD").."/"..dir)
+			--os.execute(string.format("mkdir -p %q", os.getenv("PWD").."/"..dir))
+		end
+		local hand = io.open(dent.name, "w")
+		local remaining = dent.filesize
+		while remaining > 0 do
+			local chunk = 2048
+			if chunk > remaining then chunk = remaining end
+			local buffer = assert(file:read(chunk))
+			if (#buffer ~= chunk) then
+				error("unexpected eof")
+			end
+			hand:write(buffer)
+			remaining = remaining - chunk
+		end
+		hand:close()
 	end
-	if (dent.mode & 32768 ~= 0) then
-		fwrite()
-	end
-	if (dent.filesize % 2 ~= 0) then
-		file:seek("cur", 1)
-	end
+	if dent.filesize & 1 > 0 then file:seek("cur", 1) end
 end
